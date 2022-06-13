ae1fe46bebef4550ab85d2d97731f7c79e55fbf5
brendanedelson <brendanedelson@gmail.com>
2022-06-12 22:14:26 -0400

    remove template

--- template/src/fees.rs
+++
@@ -1,9 +0,0 @@
-/// ! Traits and default implementation for paying transaction fees.
-use crate::Config;
-
-pub trait FeeCalculator<T: Config> {
-	fn match_event(
-		event: pallet_balances::Event<T>,
-		curr_block_fee_sum: &mut <T as Config>::Balance,
-	);
-}
