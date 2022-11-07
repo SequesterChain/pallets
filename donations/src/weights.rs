0b69ded5ec610d722b83a62268559a17fa9f491c
brendanedelson <brendanedelson@gmail.com>
2022-11-06 22:23:16 -0500

    rm line

--- donations/src/weights.rs
+++ donations/src/weights.rs
@@ -3,7 +3,6 @@
 //!
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
 //! DATE: 2022-10-28, STEPS: `50`, REPEAT: 20, LOW RANGE: `[]`, HIGH RANGE: `[]`
-//! HOSTNAME: `Brendans-MacBook-Pro.local`, CPU: `<UNKNOWN>`
 //! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("dev"), DB CACHE: 1024
 
 // Executed Command:
