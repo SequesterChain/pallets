4d28bbd156183029f819b85775c1f5f5a4231889
brendanedelson <brendanedelson@gmail.com>
2022-12-24 09:58:39 -0700

    remove fully qualified paths

--- donations/src/lib.rs
+++ donations/src/lib.rs
@@ -41,7 +41,6 @@
 // trait, which will check the on-chain storage for queued txn fees. If txn fees are queued,
 // they will be subsumed into a special Sequester account, and an XCM will be constructed sending
 // the queued funds to the Sequester chain.
-#![feature(more_qualified_paths)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
@@ -73,7 +72,7 @@ pub mod pallet {
         pallet_prelude::*,
         RawOrigin,
     };
-    use pallet_transaction_payment::OnChargeTransaction;
+    use pallet_transaction_payment::{Event::TransactionFeePaid, OnChargeTransaction};
     use pallet_treasury::{BalanceOf, PositiveImbalanceOf};
     use sp_runtime::{
         offchain::{
@@ -226,7 +225,7 @@ pub mod pallet {
             let mut curr_block_fee_sum: BalanceOf<T> = Zero::zero();
             for filtered_event in filtered_events {
                 match filtered_event {
-                    <pallet_transaction_payment::Event<T>>::TransactionFeePaid {
+                    TransactionFeePaid {
                         who: _,
                         actual_fee,
                         tip: _,
